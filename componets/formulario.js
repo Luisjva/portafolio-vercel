23b24166aee4cec4ea3a96fa3dbe13efe908bb12
Luisjva <luisjva16@gmail.com>
2021-03-30 07:12:44 -0400

    se cambio el form por la direccion de correo

--- componets/formulario.js
+++ componets/formulario.js
@@ -1,23 +1,37 @@
 import React from "react";
 
-export default function Formulario  {
-
-  render() {
+export default function Formulario() {
     return (
       <div className="contacto">
         <h2 id="contacta">Contacto</h2>
+        <div className="contenedor-gmail">
+           <h3 className="gmail">Luisjva16@gmail.com</h3>
+        </div>
         <style jsx>{`
-          h2 {
-            text-align: center;
-          }
-
           .contacto {
             grid-area: contacto;
             margin-top: .5rem;
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .contenedor-gmail {
+            background: linear-gradient(to right, #373b44, #4286f4);
+            padding-bottom: 4px;
+            border-radius: 4px;
+          }
+
+          h3 {
+            margin-block-start: 0;
+            margin-block-end: 0;
+            padding: 0 4px;
+            background-color: #fff;
           }
         `} </style>
       </div>
     )
-  }
 }
 
