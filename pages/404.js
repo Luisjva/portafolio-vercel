5a41a3ae2cc2b08565dcd3d4abb38fa5d72bef28
Luisjva <luisjva16@gmail.com>
2021-04-12 08:40:00 -0400

    Se puso un id en el index para poder acomodar todo

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,16 @@
-import Nav from "../componets/nav"
 import Head from "next/head"
-import Footer from "../componets/footer"
 
 export default function Custom404() {
-  return(
+  return (
     <div className="cuerpo">
       <Head>
         <title>404 | Luis Villegas</title>
       </Head>
-      <Nav />
       <main>
         <img src="/404.svg" />
         <p>La pagina que busco no existe</p>
 
       </main>
-      <Footer />
       <style jsx>{`
         .cuerpo {
           height: 96vh;
